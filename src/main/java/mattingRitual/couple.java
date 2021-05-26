464ed562a2addbbe0f138384a0ccbbc429fd0a88
philippe <bausphilippe@gmail.com>
2021-05-26 10:52:48 +0200

    Class couple.java

---
+++ src/main/java/mattingRitual/couple.java
@@ -0,0 +1,26 @@
+package mattingRitual;
+
+public class couple {
+
+    private int leftMember;
+    private int rightMember;
+
+    public couple(int leftMember, int rightMember) {
+        this.leftMember = leftMember;
+        this.rightMember = rightMember;
+    }
+
+    public void flip() {
+        int stock = leftMember;
+        leftMember = rightMember;
+        rightMember = stock;
+    }
+
+    public int getLeftMember() {
+        return leftMember;
+    }
+
+    public int getRightMember() {
+        return rightMember;
+    }
+}
