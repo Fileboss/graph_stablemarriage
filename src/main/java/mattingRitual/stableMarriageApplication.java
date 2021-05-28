029346eca478f979562826103063eea00bd85939
philippe <bausphilippe@gmail.com>
2021-05-28 08:51:46 +0200

    zouiz

--- src/main/java/mattingRitual/stableMarriageApplication.java
+++ src/main/java/mattingRitual/stableMarriageApplication.java
@@ -1,6 +1,7 @@
 package mattingRitual;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class stableMarriageApplication {
@@ -9,6 +10,17 @@ public class stableMarriageApplication {
         //Get the data
         // first not from a file
         MattingPreferences mp = new MattingPreferences();
+
+        List<Integer>[] listeAttente = new List[mp.getColumns().size()];
+        for (int i = 0; i < listeAttente.length; i++) {
+            listeAttente[i] = new LinkedList<>();
+        }
+
+        List<Integer>[] listePreferenceEleve = new List[mp.getLines().size()];
+        for (int i = 0; i < listePreferenceEleve.length; i++) {
+            listePreferenceEleve[i] = valueAsIndice(mp.getLine(i));
+        }
+
 
         Boolean studentFirst = true;
 
@@ -31,5 +43,14 @@ public class stableMarriageApplication {
 
 
 
+    }
+
+    private static List<Integer> valueAsIndice(List<Integer> inputList) {
+        List<Integer> retour = new LinkedList<>();
+        for (int i = 0; i < inputList.size(); i++) {
+            int val = i+1;
+            retour.add(inputList.lastIndexOf(val));
+        }
+        return retour;
     }
 }
