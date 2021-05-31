39d120da44bf2898bd377b0ebb8cc65bc59e463a
cedric.olender <cedric.olender@gmail.com>
2021-05-31 18:51:59 +0200

    comments in stableMarriageApplication 75%

--- src/main/java/mattingRitual/stableMarriageApplication.java
+++ src/main/java/mattingRitual/stableMarriageApplication.java
@@ -9,6 +9,7 @@ public class stableMarriageApplication {
 
     public static void main(String[] args) {
 
+        // Test if there are exactly 2 arguments
         if (args.length != 2) {
             System.out.println("ERREUR FORMAT ARGUMENTS");
             System.exit(2);
@@ -17,7 +18,7 @@ public class stableMarriageApplication {
         final String STARTER = args[1];
         boolean startWithLines = true;
 
-        //
+        // Determine if the Entities on the lines or the columns do the biding
         switch (STARTER) {
             case "lines" : startWithLines = true; break;
             case "columns" : startWithLines = false; break;
@@ -26,25 +27,26 @@ public class stableMarriageApplication {
                 System.exit(2);
         }
 
-        //Get the data
-        // first not from a file
+        //Load the data from the csv file into mp
         MattingPreferences mp = new MattingPreferences(FILE);
-        //mp = new MattingPreferences();
         System.out.println(mp);
 
         //create all the linesEntities
         Entity[] linesEntities = new Entity[mp.getLinesLabels().size()];
         for (int i = 0; i < linesEntities.length; i++) {
+            //Each entity has a different label but a same capacity
             linesEntities[i] = new Entity(mp.getLinesLabels().get(i), mp.getLinesCapacity());
         }
 
         // create all columnsEntities
         Entity[] columnsEntities = new Entity[mp.getColumnsLabels().size()];
         for (int i = 0; i < columnsEntities.length; i++) {
+            //Each entity has a different label but a same capacity
             columnsEntities[i] = new Entity(mp.getColumnsLabels().get(i), mp.getColumnsCapacity());
         }
 
-        // Assign student preference list
+        // Assign the Entities from the lines
+        // (in our case the Students) their respective preference list
         for (int i = 0; i < linesEntities.length; i++) {
             List<Integer> preferences = valueAsIndice(mp.getLine(i));
             for (Integer ind : preferences) {
@@ -52,7 +54,8 @@ public class stableMarriageApplication {
             }
         }
 
-        // Assign School preference list
+        // Assign the Entities from the columns
+        // (in our case the Schools) their respective preference list
         for (int i = 0; i < columnsEntities.length; i++) {
             List<Integer> preferences = valueAsIndice(mp.getColumn(i));
             for (Integer ind : preferences) {
@@ -60,32 +63,56 @@ public class stableMarriageApplication {
             }
         }
 
-        // algo
+        // The list of Entities that are not assigned to any other Entities
+        // or that are not assigned to their full capacity
         List<Entity> notAssignedMatters;
+
+        // The list of Entities that can't be assigned
         List<Entity> impossibleToAssignMatters = new ArrayList<>();
+
+        // The list of Entities that do the biding
         List<Entity> matters;
+
+        // The list of Entities that receive the biding and choose whom the keep
         List<Entity> matteds;
 
         // Assign the correct list depending on which Entity starts
         if (startWithLines) {
+            // The Entities on the lines start
             matters =  new LinkedList<>(Arrays.asList(linesEntities));
             matteds = new LinkedList<>(Arrays.asList(columnsEntities));
             notAssignedMatters =matters;
         } else {
+            // The Entities on the columns start
             matters = new LinkedList<>(Arrays.asList(columnsEntities));
             matteds = new LinkedList<>(Arrays.asList(linesEntities));
             notAssignedMatters =matters;
         }
 
 
+        // Number of rounds needed to reach a stable marriage
         int cptTour = 0;
-        //Algo
-        while ( ! notAssignedMatters.isEmpty()) {
 
+        // While the matters still have entities in their preference list
+        // or can still be assigned a number of time
+        // then we do one more round
+        while ( ! notAssignedMatters.isEmpty()) {
             // Biding
+
+            // the list of Entities that reached full capacity
+            // or don't have anymore Entity in their preference list
             List<Entity> toRemove = new LinkedList<>();
+
+            // We loop over each Entity that can still be assigned
             for (Entity matter : notAssignedMatters) {
+                // matter.popPrefered() -> return the next most prefered Entity
+                // .addToWaitingList(matter) -> add to the waiting list of this Entity the matter
+                // For instance if matter is a Students then
+                //      matter.popPrefered() return the next prefered school
+                //      and .addToWaitingLIst(matter) add the studens to the waiting list of the school
                 matter.popPrefered().addToWaitingList(matter);
+
+                //
                 matter.decreaseCapacity();
                 if (matter.getCapacity() == 0 || !matter.isAssignable()) {
                     toRemove.add(matter);
