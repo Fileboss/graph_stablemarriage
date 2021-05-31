d12523895632afafb616bcd050dde7b9fb5fc819
cedric.olender <cedric.olender@gmail.com>
2021-05-31 18:28:32 +0200

    comments in MattingPreferences finished

--- src/main/java/mattingRitual/MattingPreferences.java
+++ src/main/java/mattingRitual/MattingPreferences.java
@@ -14,14 +14,28 @@ import java.util.List;
 
 public class MattingPreferences {
 
+    /** Contains the names of each Entity of the lines from the csv file. */
     private final List<String> linesLabels;
+    /** One capacity for each Entity in the line of Entity from the csv. **/
     private int linesCapacity;
 
+    /** Contains the names of each Entity of the columns from the csv file. **/
     private final List<String> columnsLabels;
+    /** One capacity for each Entity in the column of Entity from the csv file. **/
     private int columnsCapacity;
 
-    private /*final*/ Couple[][] preferences ;
+    /** Contains the preferences of the Entities from the lines toward
+     * the Entities from the columns and vice versa.
+     * For instance Couple[0][1] contains the preferences of the Entities of name
+     */
+    private Couple[][] preferences ;
 
+    /** Create a MattingPreferences with the following data.
+     * 1,1 Alpha Beta Gamma
+     * A   2,2   1,1  3,3
+     * B   1,1   2,2  3,1
+     * C   1,3   2,3  3,2
+     */
     public MattingPreferences(){
         // Line
         List<String> linesStock = new ArrayList<>();
@@ -47,6 +61,13 @@ public class MattingPreferences {
         preferences = preferencesStock.clone();
     }
 
+    /** Create a MattingPreferences from a csv file of name fileName.
+     * Each element is separated with a tab.
+     * Pleased edit tab with a text editor such as sublime text
+     * (doesn't works with intellij)
+     *
+     * @param fileName the name of the csv file
+     */
     public MattingPreferences(String fileName) {
         final CSVParser parser = new CSVParserBuilder().withSeparator('\t').withIgnoreQuotations(true).build();
         List<String[]> result = new ArrayList<>();
@@ -72,28 +93,45 @@ public class MattingPreferences {
             for (int j = 1; j < result.get(i).length; j++) {
                 preferences[i-1][j-1] = new Couple(result.get(i)[j]);
             }
-
         }
-
-
     }
 
+    /** Return the list of labels of the line elements
+     *
+     * @return lineLabels
+     */
     public List<String> getLinesLabels() {
         return linesLabels;
     }
 
+    /** Return the list of labels of the column elements
+     *
+     * @return columnsLabels
+     */
     public List<String> getColumnsLabels() {
         return columnsLabels;
     }
 
+    /** Return linesCapacity
+     *
+     * @return linesCapacity
+     */
     public int getLinesCapacity() {
         return linesCapacity;
     }
 
+    /** Return columnsCapacity
+     *
+     * @return columnsCapacity
+     */
     public int getColumnsCapacity() {
         return columnsCapacity;
     }
 
+    /** Return the preferences
+     *
+     * @return preferences
+     */
     public Couple[][] getPreferences() {
         return preferences;
     }
@@ -118,6 +156,12 @@ public class MattingPreferences {
         return buildPreferences.toString();
     }
 
+    /** Return the line of index indice in the preferences array.
+     * It corresponds to the Entity of name linesLabel[indice]
+     *
+     * @param indice the index of the Entity
+     * @return the list of preferences of the Entity
+     */
     public List<Integer> getLine(int indice) {
         List<Integer> retour = new ArrayList<>();
         for (int j = 0; j < preferences[0].length; j++) {
@@ -126,13 +170,17 @@ public class MattingPreferences {
         return retour;
     }
 
+    /** Return the column of index indice in the preferences array.
+     * It corresponds to the Entity of name linesLabel[indice]
+     *
+     * @param indice the index of the Entity
+     * @return the list of preferences of the Entity
+     */
     public List<Integer> getColumn(int indice) {
         List<Integer> retour = new ArrayList<>();
         for (int i = 0; i < preferences.length; i++) {
             retour.add(preferences[i][indice].getRightMember());
         }
         return retour;
-
     }
-
 }
